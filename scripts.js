4c9069f87afbbf84c6b6c63fee312d78f748e3a2
Tushar Kudave <tusharvkudave01@gmail.com>
2025-06-29 18:55:24 +0530

    Added the comments to the main script.

--- scripts.js
+++ scripts.js
@@ -5,13 +5,14 @@ let ulEl = document.getElementById("ul-el")
 const deletebtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
 
+//To get the local storage object.
 const leadsfromLocalStorage = JSON.parse(localStorage.getItem("myleads"))
 if(leadsfromLocalStorage){
 	myleads= leadsfromLocalStorage
 	render(myleads)
 }
 
-
+//Sho the added links to the browser extension.
 function render(leads){
 	let listItems = ""
 	for(let i = 0;i<leads.length; i++){
@@ -25,6 +26,7 @@ function render(leads){
 	ulEl.innerHTML = listItems
 }
 
+//To add the link.
 inputbtn.addEventListener("click", function() {
 	myleads.push(inputEl.value)
 	inputEl.value = ""
@@ -32,6 +34,7 @@ inputbtn.addEventListener("click", function() {
 	render(myleads)
 })
 
+//To add the tab link.
 tabBtn.addEventListener("click", function(){
 	
 	chrome.tabs.query({active: true, currentWindow: true}, function(){
@@ -42,6 +45,7 @@ tabBtn.addEventListener("click", function(){
 
 })
 
+//To delete all the links.
 deletebtn.addEventListener("dblclick", function(){
 	localStorage.clear()
 	myleads =[]
